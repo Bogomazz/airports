0095d60d22d8499f0bfac1dc2f38fc1322e08ff5
Anton Bohomaz <bogomaz97@gmail.com>
2019-09-10 10:33:39 +0300

    fix error handling

--- api/utils/errorHandler.js
+++ api/utils/errorHandler.js
@@ -3,7 +3,7 @@ module.exports = (err, req, res, next) => {
   const error = {
     message: err.message,
     status: err.status || 500,
-    error: req.app.get('env') === 'development' ? err : {}
+    error: process.env.NODE_ENV === 'development' ? err : {}
   };
 
   res.status(err.status || 500);
